06fdd3aef68e70412dd6a17aa70a0d319618179d
roger778876 <rogercesar778876@gmail.com>
2017-10-18 01:09:47 -0400

    update ll.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct song_node { 
   char name[256];
@@ -26,23 +27,36 @@ struct song_node *insert_front(struct song_node *node, char *n, char *a) {
 
 // returns pointer to new node
 struct song_node *insert_order(struct song_node *node, char *n, char *a) {
-  if (node == NULL) {
+  if (!node) {
     return insert_front(node, n, a);
   }
 
   else {
-    struct song_node *front = node;
+    struct song_node *prev = NULL;
+    struct song_node *curr = node;
+
+    while (curr) {
+      if (strcmp(curr->name, n) > 0) {
+        if (prev == NULL) {
+          return insert_front(curr, n, a);
+        }
+        else {
+          prev->next = insert_front(curr, n, a);
+          return node;
+        }
+      }
+      else {
+        prev = curr;
+        curr = curr->next;
+      }
+    }
+
     struct song_node *new = (struct song_node *) malloc(sizeof(struct song_node));
     strncpy(new->name, n, 256);
     strncpy(new->artist, a, 256);
-    // printf("new: %s, %s\n", new->name, new->artist);
-    printf("%d\n", strcmp(node->name, new->name));
-    while (strcmp(node->name, new->name) != 1) {
-      node = node->next;
-    }
-    new->next = node->next;
-    node->next = new;
-    return front;
+    prev->next = new;
+
+    return node;
   }
 }
 
@@ -57,6 +71,40 @@ struct song_node *find_song(struct song_node *node, char *n, char *a) {
   }
   return 0;
 }
+
+struct song_node *find_artist(struct song_node *node, char *a) {
+  while (node != NULL) {
+    if (strcmp(node->artist, a) == 0) {
+      return node;
+    }
+    else {
+      node = node->next;
+    }
+  }
+  return 0;
+}
+
+int list_len(struct song_node *node) {
+  int output = 0;
+  while (node) {
+    output++;
+    node = node->next;
+  }
+  return output;
+}
+
+struct song_node *random_song(struct song_node *node) {
+  int range = list_len(node) - 1;
+  srand(time(NULL));
+  int random = rand() % (range + 1);
+  while (random > 0) {
+    random--;
+    node = node->next;
+  }
+  return node;
+}
+
+
 
 struct node *free_list(struct song_node *node) {
   struct song_node *first = node;
@@ -74,17 +122,29 @@ void main() {
   struct song_node *list = NULL;
   print_list(list);
 
-  
-
-  list = insert_front(list, "c", "artist3");
+  list = insert_order(list, "c", "artist3");
   print_list(list);
+  printf("\n");
 
-  list = insert_front(list, "b", "artist2");
+  list = insert_order(list, "a", "artist1");
   print_list(list);
+  printf("\n");
 
-  list = insert_front(list, "a", "artist1");
+  list = insert_order(list, "b", "artist2");
   print_list(list);
+  printf("\n");
+
+  list = insert_order(list, "d", "artist4");
+  print_list(list);
+  printf("\n");
 
   printf("pointer: %p\n", find_song(list, "b", "artist2"));
-  
+  printf("next song: %s\n", find_song(list, "b", "artist2")->next->name);
+
+  printf("pointer: %p\n", find_artist(list, "artist4"));
+  printf("next song: %s\n", find_artist(list, "artist4")->next->name);
+
+  printf("len: %d\n", list_len(list));
+
+  printf("random: %s by %s\n", random_song(list)->name, random_song(list)->artist);
 }
